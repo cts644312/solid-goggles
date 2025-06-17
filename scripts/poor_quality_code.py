ba507c9210fdc43670cd976e00cecca7c1eebdd8
Sayan Bose <sayan.bose@cognizant.com>
2025-06-17 13:13:47 +0530

    Create poor_quality_code.py

---
+++ scripts/poor_quality_code.py
@@ -0,0 +1,24 @@
+def example_function():
+    # Unused variable
+    unused_var = 42
+
+    # Deeply nested logic
+    for i in range(5):
+        if i % 2 == 0:
+            for j in range(3):
+                if j % 2 == 0:
+                    for k in range(2):
+                        if k % 2 == 0:
+                            print("Deeply nested logic")
+
+    # Duplicated code
+    print("This is duplicated code")
+    print("This is duplicated code")
+
+    # Lack of documentation
+    a = 10
+    b = 20
+    c = a + b
+    print(c)
+
+example_function()
